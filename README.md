e377fa1e85f6fe165553d123fdc9d46c7ff67b58
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-03-19 23:06:10 -0700

    final version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # MySQL-Node-HW
+
+## Bamazon CLI Storefront
+This assignment is a Command-Line Interface (CLI)Storefront with MySQL backend.
+
+
+## Modules
+This CLI utilizes the following npm modules:
+
+[inquirer](https://www.npmjs.com/package/inquirer) (allows storefront to prompt user about what they want)
+
+[mysql](https://www.npmjs.com/package/mysql) (allows node, JavaScript, and MySQL to all connect/work together)
+
+
+## Application Description
+This app displays a selection of products (along with their IDs), the department they're in, their price, and the stock quantity of each corresponding product.  Users are prompted whih product they would like to purchase based on that product's ID. They are then prompted how many they would like to purchase. If there is enough of that product in stock, the app then displays the selection of products again, with updated stock quantities. Is there is not enough product in stock, the propting starting over again. Users may quit at any time with "Q" or select "Quit" after making a successful purchase.
+
+
+## Bamazon Demo
+[Bamazon Demo](https://drive.google.com/file/d/1BBnl0b2z_s1K1ikOGXG9Tv5R7WdsAREF/view)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(error) {
+connection.connect(function (error) {
   if (error) throw error;
   // console.log("connected as id " + connection.threadId);
   displayProducts();
@@ -18,35 +18,78 @@ connection.connect(function(error) {
 
 // read/SELECT (display) your table's content
 function displayProducts() {
-  connection.query("SELECT * FROM products", function (error, results, fields) {
+  connection.query("SELECT * FROM products", function (error, results) {
     if (error) throw error;
     console.log("\n~~~~~~~~~~~~~~~~~~~~Bamazon Products~~~~~~~~~~~~~~~~~~~~");
     for (var i = 0; i < results.length; i++) {
-      console.log("Product ID: " + results[i].item_id + " | Product: " + results[i].product_name + " | Price: " + results[i].price);
+      console.log("Product ID: " + results[i].item_id + " || Product: " + results[i].product_name + " || Dapartment: " + results[i].department_name + " || Price: " + results[i].price + " || In Stock: " + results[i].stock_quantity);
     }
     console.log("~~~~~~~~~~~~~~~~~~~~Bamazon Products~~~~~~~~~~~~~~~~~~~~\n");
-    chooseProduct();
+    chooseProduct(results);
   });
 };
 
-var chooseProduct = function() {
-  inquirer.prompt({
+var chooseProduct = function (results) {
+  inquirer.prompt([{
     name: "productChoice",
     type: "input",
-    message: "Please input the ID of the product you wish to purchase: "
-  }).then(function(input) {
-    console.log(input.productChoice)
-    var query = connection.query("SELECT item_id, product_name FROM products WHERE ?",
-    {
-      item_id: input.productChoice
-    }, function (error, results, fields) {
-      // console.log(error);
-      // console.log(results);
+    message: "\nPlease input the ID of the product you wish to purchase: ",
+    validate: function (value) {
+      if (isNaN(value) == false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }, {
+    name: "quantity",
+    type: "input",
+    message: "\nPlease input the quantity you would like to purchase: ",
+    validate: function (value) {
+      if (isNaN(value) == false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }]).then(function (answer) {
+    var correct = false;
+    if (answer.productChoice.toUpperCase() == "Q") {
+      // connection.end();
+      process.exit();
+    }
+    connection.query("SELECT * FROM products WHERE ?", {
+      item_id: answer.productChoice
+    }, function (error, res) {
+      // console.log(res);
+      correct = true;
+      var newStockQuantity = res[0].stock_quantity - answer.quantity
+      console.log(newStockQuantity);
+      if (newStockQuantity >= 0) {
+        connection.query("UPDATE products SET stock_quantity = '" + newStockQuantity + "'WHERE item_id = '" + answer.productChoice + "'", function (error, results2) {
+          console.log("Product Bought!");
+          areYouDone();
+        })
+      } else {
+        console.log("\nSorry, there is not enough in stock. Please choose another quantity or product.\n");
+        chooseProduct();
+      }
     })
-    console.log(query);
-  })
+  });
 }
 
-// The app should then prompt users with 2 messages:
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
+function areYouDone() {
+  inquirer.prompt([{
+    name: "userDone",
+    type: "list",
+    message: "\nWhat would you like to do next?",
+    choices: ["Purchase another item", "Quit"]
+  }]).then(function(finalAnswer) {
+    if (finalAnswer.userDone === "Purchase another item") {
+      displayProducts();
+    } else {
+      console.log("\nThanks for your business!");
+      process.exit();
+    }
+  })
+}
